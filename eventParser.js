b12b6ad816a2391ef2a5c0aa153df2a54b69966e
Harshil Shah <harshilhshah1995@gmail.com>
2016-01-01 09:06:23 -0500

    getting events from firebase now

--- eventParser.js
+++ eventParser.js
@@ -1,141 +1,111 @@
-  var accessToken = "CAANjBmdVSEcBACyfLUuGiZBkZBZCCI1wWub5T17ZCtzdyKLXV8QU4kA3a9JCDtfDxz3LmQZCRZA0llSpg3l5JthypVRBbwJSeMlUmyZCTCZAbT4SZBn6HDUUrB4omEQdNoYxxA1u6ql1ZBKCiwA5AGBMhaeJVkCaNyfZBfHSTweVtdQWFXZBLM2xDhrXErssZAZBTQJmWCQ8sfc8uZAzwZDZD";
   var def_img = "http://previews.123rf.com/images/carmenbobo/carmenbobo1405/carmenbobo140500482/28389907-Stamp-with-text-free-food-inside-vector-illustration-Stock-Vector.jpg";
   var myFirebaseRef = new Firebase("https://crackling-heat-4631.firebaseio.com/events");
+  var myFBFirebaseRef = new Firebase("https://crackling-heat-4631.firebaseio.com/fb");
+  var clRSSRef = new Firebase("https://crackling-heat-4631.firebaseio.com/collegiatelinkrss");
   var timeParser = new chrono.Chrono();
   var today = moment().format("YYYY-MM-DD");
   var eventData = [];
-  var param = "/events?fields=name,description,start_time,end_time,place,picture.type(large){url}&since=" + today;
-  var fb_urls = ['/RUPAPresents','/RUOCSA','/ruaacc','/youth.eagleton','/Cookiesncrepesnb','/rutgersmad',
-            '/Rutgers.psa','/ruveg','/RUSURErutgers','/me','/oneatrutgers'];
   var food_tags = [ "appetizer", "snack", "pizza", "lunch", "dinner", "breakfast", "meal", "candy", 
-            "drinks","punch", "serving", "pie",  "cake", "soda", "chicken", "wings", "burger",
+            "drinks", "punch", "serving", "pie",  "cake", "soda", "chicken", "wings", "burger",
             "burrito", "bagel", "poporn", " ice ", "cream", "donut", "beer", "free food", 
             "subs", "hoagie", "sandwich", "turkey", "supper", "brunch", "takeout", "refreshment",
-            "beverage", "cookie", "brownie", "chips", "soup", "grill", "bbq", "barbecue"
-        ];
-
-  FB.init({
-          appId      : '953304084727879',
-          xfbml      : true,
-          version    : 'v2.4'
-  });
-
-  FB.getLoginStatus(function(response) {
-    if (response.status === 'connected') {
-        accessToken = response.authResponse.accessToken;
-    }else if (response.status !== 'not_authorized') {
-      FB.login(function(response) {
-        if (response.authResponse) {
-          accessToken = response.authResponse.accessToken;
-        }
-      },true, {scope: 'user_events'});
-   } 
-  });
-
-  myFirebaseRef.on("child_added", function(snapshot) {
-    var data = snapshot.val();
-    if(getTags(data.body).length < 1) return;
-    var time = timeParser.parseDate(data.body,moment(data.created));
-    if(time == null || moment(time).format("YYYY-MM-DD") < today) return;
-    var timeChronoSt = moment(time).format("YYYY-MM-DD hh:mm:ss");
-    time = moment(time).format("dddd, MMM D (h:mm A)");
-    eventData = [];
-    eventData.push([data.created,data.subject,def_img,time,"Check description",data.body,timeChronoSt,timeChronoSt]);
-    changeURL();
-  });
-
-  for (var i = 0; i < fb_urls.length; i++) {
-    FB.api(fb_urls[i]+param,{access_token: accessToken }, function(response) {
-      loopOverResponse(response.data);
-    });
-  };
+            "beverage", "cookie", "brownie", "chips", "soup", "grill", "bbq", "barbecue", "tacos"
+        ];  
 
   function getTags(str){
     var ret = "";
     for(var i = 0; i < food_tags.length; i++){
-      if(str.toLowerCase().indexOf(food_tags[i]) !== -1){
+      if(str != undefined && str.toLowerCase().indexOf(food_tags[i]) !== -1){
         ret += "#" + food_tags[i] + " ";
       }
     }
     return ret;
   }
 
-  function loopOverResponse(data){
-    if(data === undefined) return;
-    for (var i = 0; i < data.length; i++){
-      if(getTags(data[i].description).length < 1) continue;
-      var location = (data[i].place === undefined || data[i].place.location === undefined) ? " " : data[i].place.location.street + ", " + data[i].place.location.city + " " + data[i].place.location.state;
-      var time = moment(data[i].start_time).format("dddd, MMM D (h:mm A");
-      var timeChronoSt = moment(data[i].start_time).format("YYYY-MM-DD hh:mm:ss");
-      var timeChronoEn = (data[i].end_time) ? moment(data[i].end_time).format("YYYY-MM-DD hh:mm:ss") : timeChronoSt;
-      time += (data[i].end_time) ? " - " + moment(data[i].end_time).format("h:mm A)") : ")";
-      var img_url = data[i].picture.data.url;
-      eventData.push([data[i].id.toString(),data[i].name,img_url,time,location,data[i].description,timeChronoSt,timeChronoEn]);
-    }
-  }
-  // get the upcoming event link and using that link find the event info.
-  function makeAjaxReq(org_url,callback){
-  $.ajax({
-      type: 'GET',
-      url: org_url,
-      dataType: 'html',
-      success: callback
+  myFirebaseRef.on("child_added", function(snapshot) {
+    var data = snapshot.val();
+    if(getTags(data.body).length < 1) return;
+    var time = timeParser.parseDate(data.body,moment(data.created));
+    if(time == null || moment(time).format("YYYY-MM-DD") < today) return;
+    var timeChronoSt = moment(time).format("YYYY-MM-DD hh:mm:ss A");
+    time = moment(time).format("dddd, MMM D (h:mm A)");
+    eventData.push([data.created,data.subject,def_img,time,"Check description",data.body,timeChronoSt,timeChronoSt]);
+    changeDisplay();
+  });
+
+  myFBFirebaseRef.on("child_added", function(snap) {
+    snap.forEach(function(dataPair) {
+      var data = dataPair.val();
+      if(getTags(data.description).length > 1) {
+        var location = (data.place === undefined || data.place.location === undefined) ? " " : data.place.location.street + ", " + data.place.location.city + " " + data.place.location.state;
+        var time = moment(data.start_time).format("dddd, MMM D (h:mm A");
+        var timeChronoSt = moment(data.start_time).format("YYYY-MM-DD hh:mm:ss A");
+        var timeChronoEn = (data.end_time) ? moment(data.end_time).format("YYYY-MM-DD hh:mm:ss A") : timeChronoSt;
+        time += (data.end_time) ? " - " + moment(data.end_time).format("h:mm A)") : ")";
+        var img_url = data.picture.data.url;
+        eventData.push([data.id,data.name,img_url,time,location,data.description,timeChronoSt,timeChronoEn]);
+        changeDisplay();
+      }
     });
-  }
+  });
 
+  ruRSSRef.on("child_added", function(snap) {
+    var data = snap.val();
+    if(getTags(data.description).length < 1) return;
+    var time = moment(data.startTime,"YYYY-MM-DD hh:mm:ss ddd").format("dddd, MMMM D, YYYY (h:mm A")
+      + moment(data.endTime,"YYYY-MM-DD hh:mm:ss ddd").format(" - h:mm A)");
+    var timeChronoSt = moment(data.startTime,"YYYY-MM-DD hh:mm:ss ddd").format("YYYY-MM-DD hh:mm:ss A");
+    var timeChronoEn = moment(data.endTime,"YYYY-MM-DD hh:mm:ss ddd").format("YYYY-MM-DD hh:mm:ss A");
+    var location = data.location + ", " + data.campus;
+    eventData.push([snap.key(),data.title,def_img,time,location,data.description,timeChronoSt,timeChronoEn]); 
+    changeDisplay();
+  });
 
-  function findEvents(data){
-    var itemList = $(data).find('item');
-    for(var i = 0; i < itemList.length; i++){
-      var item = $(itemList[i]);
-      var event_title = item.contents()[3].nodeValue;
-      if (event_title == null) event_title = "No Title";
-      var descr_text = item.find('description').text();
-      if(getTags(descr_text).length < 1) continue;
-      var img_url = item.find('enclosure').attr('url');
-      img_url = (img_url) ? img_url : def_img;
-      var time = $.parseHTML(descr_text)[0].innerText;
-      var timeChronoSt = today;
-      var timeChronoEn = today;
-      if(time.indexOf(") -") !== -1){
+  clRSSRef.on("child_added", function(snap) {
+    var data = snap.val();
+    if(getTags(data.description).length < 1) return;
+    var event_title = data.title;
+    if (event_title == null) event_title = "No Title";
+    var img_url = def_img;
+    var descr_text = $('<div/>').html(data.description).text();
+    var time = $.parseHTML(descr_text)[0].innerText;
+    var timeChronoSt = today;
+    var timeChronoEn = today;
+    if(time.indexOf(") -") !== -1){
         var t_split = time.split(') - ');
-        timeChronoSt = moment(t_split[0],"dddd, MMMM D, YYYY (h:mm A").format("YYYY-MM-DD hh:mm:ss");
-        timeChronoEn = moment(t_split[1],"dddd, MMMM D, YYYY (h:mm A)").format("YYYY-MM-DD hh:mm:ss");
+        timeChronoSt = moment(t_split[0],"dddd, MMMM D, YYYY (h:mm A").format("YYYY-MM-DD hh:mm:ss A");
+        timeChronoEn = moment(t_split[1],"dddd, MMMM D, YYYY (h:mm A)").format("YYYY-MM-DD hh:mm:ss A");
         time = moment(t_split[0],"dddd, MMMM D, YYYY (h:mm A").format("dddd, MMM D (h:mm A)") + 
           moment(t_split[1],"dddd, MMMM D, YYYY (h:mm A)").format(" - MMM D (h:mm A)");
-      }else if(time.indexOf("M -")){
+    }else if(time.indexOf("M -")){
         var t_split = time.split(' - ');
-        timeChronoSt = moment(t_split[0],"dddd, MMMM D, YYYY (h:mm A").format("YYYY-MM-DD hh:mm:ss");
-        timeChronoEn = moment(t_split[1],"h:mm A)").format("YYYY-MM-DD hh:mm:ss");
+        timeChronoSt = moment(t_split[0],"dddd, MMMM D, YYYY (h:mm A").format("YYYY-MM-DD hh:mm:ss A");
+        timeChronoEn = moment(t_split[1],"h:mm A)").format("YYYY-MM-DD hh:mm:ss A");
         time = moment(t_split[0],"dddd, MMMM D, YYYY (h:mm A").format("dddd, MMM D (h:mm A") +
           moment(t_split[1],"h:mm A)").format(" - h:mm A)");
-      }else{
+    }else{
         time = moment(time,"dddd, MMMM D, YYYY (h:mm A)").format("dddd, MMM D (h:mm A)");
-        timeChronoSt = moment(time,"dddd, MMMM D, YYYY (h:mm A)").format("YYYY-MM-DD hh:mm:ss");
+        timeChronoSt = moment(time,"dddd, MMMM D, YYYY (h:mm A)").format("YYYY-MM-DD hh:mm:ss A");
         timeChronoEn = timeChronoSt;
-      }
-      var location = $.parseHTML(descr_text)[2].innerText.replace("Location: ","");
-      var description = $.parseHTML(descr_text)[4].innerText;
-      var eventItem = ["0",event_title,img_url,time,location,description,timeChronoSt,timeChronoEn];
-      eventData.push(eventItem);
     }
-    changeURL();
-  }
+    var location = $.parseHTML(descr_text)[2].innerText.replace("Location: ","");
+    var description = $.parseHTML(descr_text)[4].innerText;
+    eventData.push([snap.key(),event_title,img_url,time,location,description,timeChronoSt,timeChronoEn]); 
+    changeDisplay();
+  });
 
-  function changeURL(){
-    var temp = [];
-    $.when.apply($, temp).then(function() {
-             var xg=arguments; // The array of resolved objects as a pseudo-array
-    
-             for (var i = xg.length - 1; i >= 0; i--) {
-               eventData.push(xg[i]);
-             };  
-    })   
+  function changeDisplay(){
+    $('#event_box').html("");
     eventData.sort(function(a,b){
       var da = (a[3].indexOf(') -') != -1) ? new Date(a[3]) : new Date(a[3].split(' - ')[0]);
       var db = (b[3].indexOf(') -') != -1) ? new Date(b[3]) : new Date(b[3].split(' - ')[0]);      
       return da - db;
     });
+    for (var i = 1; i < eventData.length; i++) {
+      if(eventData[i][1] == eventData[i-1][1]){
+        eventData.splice(i,1);
+      }
+    };
     display();
   }
 
@@ -159,4 +129,4 @@
     $(".div").hide();
   }
 
-  makeAjaxReq("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%20%3D%20%22https%3A%2F%2Frutgers.collegiatelink.net%2FEventRss%2FEventsRss%22%20and%20xpath%3D%22%2F%2Fitem%22&diagnostics=true",findEvents);
+
