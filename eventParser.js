d82dd8c3d2d15ccb50e54036cee780df1f20fa21
Harshil Shah <harshilhshah1995@gmail.com>
2016-01-12 23:14:17 -0500

    fixed the addToCal bug

--- eventParser.js
+++ eventParser.js
@@ -132,11 +132,12 @@
   }
 
   function addToCal() {
-    if (window.addtocalendar)if(typeof window.addtocalendar.start == "function")return;
-    if (window.ifaddtocalendar == undefined) { window.ifaddtocalendar = 1;
+    if (window.ifaddtocalendar == 1) { 
+      $("#addToCal").remove();
+    }
+      window.ifaddtocalendar = 1;
         var d = document, s = d.createElement('script'), g = 'getElementsByTagName';
-        s.type = 'text/javascript';s.charset = 'UTF-8';s.async = true;
+        s.type = 'text/javascript';s.charset = 'UTF-8';s.async = true;s.id= 'addToCal';
         s.src = ('https:' == window.location.protocol ? 'https' : 'http')+'://addtocalendar.com/atc/1.5/atc.min.js';
         var h = d[g]('body')[0];h.appendChild(s); 
-    }
   }
