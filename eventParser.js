1761b7c114f99e2970e250599e2daeb6224bbf3a
Harshil Shah <harshilhshah1995@gmail.com>
2017-03-04 01:25:39 -0800

    tiny change

--- eventParser.js
+++ eventParser.js
@@ -134,6 +134,7 @@ function display(){
   }
   $(".div").hide();
   addToCal();
+  (function(d,e,s){if(d.getElementById("likebtn_wjs"))return;a=d.createElement(e);m=d.getElementsByTagName(e)[0];a.async=1;a.id="likebtn_wjs";a.src=s;m.parentNode.insertBefore(a, m)})(document,"script","//w.likebtn.com/js/w/widget.js");
 }
 
 function addToCalButtonClickListener(){ ga('send', 'event', 'Button', 'Click', 'Add to Calendar');}
